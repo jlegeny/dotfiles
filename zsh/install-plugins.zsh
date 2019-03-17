597aa4fd97775d7a5e89802079f48855d955df4d
Jozef Legény <jozef.jr@legeny.sk>
2019-03-17 12:20:00 +0100

    Initial commit

---
+++ zsh/install-plugins.zsh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ZSH="${HOME}/.oh-my-zsh"
+
+curl -L https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/zsh/_docker-compose > "${ZSH}/custom/completion/docker-compose.zsh"
+
+
+
