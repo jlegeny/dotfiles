189eba35d93a6a10bc4663706ed491f5e5a79012
Jozef Legény <jozef@legeny.fr>
2019-12-19 14:07:23 +0100

    Add a rudimentary install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd $HOME
+
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Oh My Zsh Syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
