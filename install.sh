c4da82498ea9b8be4b5f03f90a2d266d95ba8de5
Jozef Legény <yozy@helios.home>
2020-03-14 16:42:43 +0100

    Install ohmyzsh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 cd $HOME
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
