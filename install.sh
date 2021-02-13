fca6ba9dc89824455a2fb125fe2090a997310b78
Jozef Legeny <jozef@legeny.fr>
2021-02-13 19:33:52 +0100

    Install: Install zsh and link config files

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 cd $HOME
 
+sudo apt install zsh
+
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -18,3 +22,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+mkdir -p ~/.config/nvim
+
+ln -s ${HOME}/dotfiles/vim/vimrc ${HOME}/.config/nvim/init.vim
+ln -s ${HOME}/dotfiles/zsh/zshrc ${HOME}/.zshrc
